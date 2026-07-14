3331d80c49d33ee2a29a054621b99ac2e5e90975
Aya Alamir <aalamir@arabdt.com>
2026-07-14 19:32:22 +0300

    Apply EF Core migrations on startup (auto-create DB)

--- HRSystem.API/HRSystem.API/Program.cs
+++ HRSystem.API/HRSystem.API/Program.cs
@@ -1,11 +1,11 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 
 namespace HRSystem.API
 {
@@ -13,7 +13,26 @@ namespace HRSystem.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            // Ensure database is created and migrations are applied on startup
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<HRSystem.Infrastructure.Persistence.AppDbContext>();
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<Microsoft.Extensions.Logging.ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while migrating or initializing the database.");
+                    throw;
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
